1c87f489a09d29cce7588084ee47823cbbe4c6bd
Jamila Ismayilzada <jamila.ismayilzada@code.local>
2023-05-08 13:38:55 +0400

    custom list

---
+++ Labday6/Models/CustomList.cs
@@ -0,0 +1,56 @@
+﻿namespace Labday6.Models
+{
+    public class CustomList<T>
+    {
+        public T[] Values;
+        public int Count { get; private set; }
+        public int Capacity
+        {
+            get
+            {
+                return Capacity;
+            }
+
+            set
+            {
+                if (value < Count)
+                {
+                    throw new Exception("Current capacity can not be less than size");
+                }
+                else
+                {
+                    Console.WriteLine( "worked");
+                    Capacity = value;
+                }
+
+            }
+            //get; set;
+        }
+        public CustomList()
+        {
+            Values = new T[0];
+            Count = 0;
+            Capacity = 0;   
+        }
+
+        public void Add(T value)
+        {
+            if (Values.Length == 0 && Capacity==0)
+            {
+
+                Capacity = 4;  
+            }
+            else
+            {
+              
+                if (Count == Capacity)
+                {
+                    Capacity = Capacity * 2;
+                }
+            }
+            Array.Resize(ref Values, Capacity);
+            Values[Count] = value;
+            Count++;
+        }
+    }
+}
---
+++ Labday6/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Labday6.Models;
+
+Console.WriteLine( "hi");
+//CustomList<int> customList = new CustomList<int>();
+
+
+//customList.Add(1);
+//customList.Add(2);
+//customList.Add(3);
+//customList.Add(3);
+
+
+//customList.Capacity = 3;
+
+
